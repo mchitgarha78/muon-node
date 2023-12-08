90350b7e7a044c06bd1f3a15310c3495e44b79fa
m.chitgarha78 <m.chitgarha78@gmail.com>
2023-12-08 12:21:42 +0330

    Add node_process. Add DKG-list-loop.

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-from common.node_info import NodeInfo
-from common.node_data_manager import NodeDataManager
-from common.validators import Validators
-
 from muon_frost_py.common.configuration_settings import ConfigurationSettings
-from muon_frost_py.node.node import Node
-
-from node_config import SECRETS
-
+from node_process import NodeProcess
 import logging
 import sys
 import trio
@@ -14,14 +7,8 @@ import trio
 
 
 
-async def run(node_number: int) -> None:
-    node_info = NodeInfo()
-    id = node_info.get_all_nodes()[node_number]
-    data_manager = NodeDataManager()
-    node = Node(data_manager, node_info.lookup_node(id), SECRETS[id], node_info, 
-                Validators.caller_validator, Validators.data_validator)
-    await node.run()
 
+ 
 if __name__ == "__main__":
 
 
@@ -33,8 +20,11 @@ if __name__ == "__main__":
     sys.set_int_max_str_digits(0)
 
     node_number = int(sys.argv[1])
+    registry_url = ''
+    node_process = NodeProcess(registry_url)
+    
     try:
         # Run the libp2p node
-        trio.run(run, node_number)
+        trio.run(node_process.run, node_number)
     except KeyboardInterrupt:
         pass
