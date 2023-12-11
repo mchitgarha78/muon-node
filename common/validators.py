d1781a51851a1d285e18353d22a38df570be9dec
m.chitgarha78 <m.chitgarha78@gmail.com>
2023-12-11 10:17:43 +0330

    Add data validator abstracts.

--- common/validators.py
+++ common/validators.py
@@ -4,6 +4,7 @@ from muon_frost_py.abstract.node.validators import Validators
 from libp2p.typing import TProtocol
 from types import FunctionType
 from node_config import VALIDATED_CALLERS
+import requests
 
 class NodeValidators(Validators):
     def __init__(self) -> None:
@@ -14,7 +15,13 @@ class NodeValidators(Validators):
         if protocol in VALIDATED_CALLERS.get(sender_id, []):
             return True
         return False
-        
+    
     @staticmethod
-    def data_validator(self, sign_function: FunctionType, commitment_list: Dict, input_data: Dict):
-        pass
+    def data_validator(self, input_data: Dict):
+        url = 'http://127.0.0.1:6000'
+        headers = {
+            "Content-Type": "application/json"
+        }
+        result = requests.post(url=url, headers = headers, data = input_data).json()
+        return result
+        
